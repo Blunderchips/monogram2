fb1a32561c684ea691d6ed8974a57030f3749ca7
Matthew Van der Bijl <matthewvdbijl@gmail.com>
2021-12-13 07:30:16 +0100

    feat: Create list item component (#35)
    - Generate new component
    - Null check on document item
    - Update home page where needed

---
+++ src/app/document-list-item/document-list-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MnDocument } from '../state/monogram.model';
+
+@Component({
+  selector: 'app-document-list-item',
+  templateUrl: './document-list-item.component.html',
+  styleUrls: ['./document-list-item.component.scss']
+})
+export class DocumentListItemComponent implements OnInit {
+
+  /**
+   * Document from storage. Cannot be null.
+   */
+  @Input() document: MnDocument;
+
+  ngOnInit(): void {
+    if (!this.document) {
+      throw new Error('Document data cannot be undefined on list item'); // illegal argument exception
+    }
+  }
+
+}
