2d6d9fe7bda3f8090f3b6fb2520f8612fabad4c8
Matthew Van der Bijl <matthewvdbijl@gmail.com>
2021-12-19 20:33:37 +0100

    tests: Update failing test specs
    - Import dialogue test mocks where needed

--- src/app/services/prompt/prompt.service.spec.ts
+++ src/app/services/prompt/prompt.service.spec.ts
@@ -1,12 +1,29 @@
 import { TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { ConfirmationDialogue } from '../../component/confirmation-dialogue';
 import { PromptService } from './prompt.service';
 
 describe('PromptService', () => {
 
   let service: PromptService;
 
+  const dialogueData: ConfirmationDialogue = {
+    title: 'test title',
+    message: 'test message',
+  };
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        MatDialogModule,
+      ],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA, useValue: dialogueData
+        },
+        { provide: MatDialogRef, useValue: {} },
+      ],
+    });
     service = TestBed.inject(PromptService);
   });
 
