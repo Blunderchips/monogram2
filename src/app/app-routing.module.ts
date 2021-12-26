c4bc9941db46365fac521d6d70fd259f3641345a
Matthew Van der Bijl <matthewvdbijl@gmail.com>
2021-12-26 10:34:31 +0100

    tests: Flesh out nav-item component test spec

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,32 +5,36 @@ import { HomeComponent } from './component/home';
 import { DisplayComponent } from './component/display';
 import { SettingsComponent } from './component/settings';
 
-const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'home',
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
-    path: 'document/:id',
-    component: DocumentComponent,
-  },
-  {
-    path: 'settings/:id',
-    component: SettingsComponent,
-  },
-  {
-    path: 'reader/:id',
-    component: DisplayComponent,
-  },
-];
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)], exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(AppRoutingModule.routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule {
+  static routes: Routes = [
+    {
+      path: '',
+      pathMatch: 'full',
+      redirectTo: 'home',
+    },
+    {
+      path: 'home',
+      component: HomeComponent,
+    },
+    {
+      path: 'document/:id',
+      component: DocumentComponent,
+    },
+    {
+      path: 'settings/:id',
+      component: SettingsComponent,
+    },
+    {
+      path: 'reader/:id',
+      component: DisplayComponent,
+    },
+  ];
 }
