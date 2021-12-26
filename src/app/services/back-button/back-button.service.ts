07c4655d2c1f44e87c44cd3c97a41c0c859e84b2
Matthew Van der Bijl <matthewvdbijl@gmail.com>
2021-12-26 17:44:01 +0100

    Revert "fix: Debug back button service initialisation"
    This reverts commit b5baa8ac997668d86d4bb59dfbacad00161e12d9.

--- src/app/services/back-button/back-button.service.ts
+++ src/app/services/back-button/back-button.service.ts
@@ -1,10 +1,10 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { Injectable, OnDestroy, OnInit } from '@angular/core';
 import { App } from '@capacitor/app';
 import { PluginListenerHandle } from '@capacitor/core';
 import { Observable, Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-export class BackButtonService implements OnDestroy {
+export class BackButtonService implements OnInit, OnDestroy {
 
   /**
    * Back button event subscription.
@@ -19,11 +19,14 @@ export class BackButtonService implements OnDestroy {
 
   constructor() {
     this.subject = new Subject<void>(); // this needs to be defined
-    this.listener = App?.addListener('backButton', () => this.subject.next(undefined));
   }
 
   get event(): Observable<void> {
     return this.subject.asObservable();
+  }
+
+  ngOnInit(): void {
+    this.listener = App?.addListener('backButton', () => this.subject.next(undefined));
   }
 
   async ngOnDestroy(): Promise<void> {
